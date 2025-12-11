969436c6fbe9826538a09cb96728e46df0e46b85
Phillip Wagner <m0ssyblue7@gmail.com>
2025-12-11 14:30:19 -0800

    START: Working on Milestone 1.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-int main () {
-    
+//Milestone 1: Using a struct, code a coffee booth queue simulation that uses a linked list as its core data structure. 
+// The linked list's struct should be a node that contains the customer's name and drink order. 
+// Create data arrays with names and drink orders from which to randomly create customer data. 
+// You may use an LLM for creating these data sets. 
+int main() {
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    const string NAMES[] = {
+        //names from LLM
+      "Alex", "Jordan", "Taylor", "Morgan", "Riley",
+     "Casey", "Sam", "Jamie", "Drew", "Quinn"  
+    };
+    const int NAME
 }
